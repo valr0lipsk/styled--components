574e86bd3ec4b47a76e01c45b39829b42e3ac68e
valeriya.lipskaya <valeriya.lipskaya@softclub.by>
2023-02-17 11:28:40 +0300

    Fix flex component.

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import styled, { StyledComponent, ThemedStyledProps } from "styled-components";
-import Wrapper from './helpers/Wrapper';
+import styled from "styled-components";
 
-interface Props extends React.PropsWithChildren {
+interface StyledProps {
   direction?: string;
   items?: string;
   justify?: string;
   margin?: string;
 }
 
-const StyledFlex = styled(Wrapper)`
+const StyledFlex = styled.div<StyledProps>`
 display: flex;
 flex-direction: ${props => props.direction || 'row'};
 justify-content: ${props => props.justify || 'stretch'};
@@ -17,7 +16,7 @@ align-items: ${props => props.items || 'stretch'};
 margin: ${({margin}) => margin || '0'};
 `
 
-const Flex: React.FC<Props> = (props) => {
+const Flex: React.FC<StyledProps & React.PropsWithChildren> = (props) => {
   return (
     <StyledFlex {...props}/>
   )
